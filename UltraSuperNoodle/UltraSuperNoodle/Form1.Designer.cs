901b3f09606ad9a817e4042ebb6f801b91bec843
avais88786 <avaisuddin.mohammad@outlook.com>
2014-12-02 22:10:59 +0000

    UltraSuperNoodle

---
+++ UltraSuperNoodle/UltraSuperNoodle/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace UltraSuperNoodle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxAppName = new System.Windows.Forms.ComboBox();
+            this.comboBoxDbNames = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_save = new System.Windows.Forms.Button();
+            this.comboBoxUserNames = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxAppName
+            // 
+            this.comboBoxAppName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAppName.FormattingEnabled = true;
+            this.comboBoxAppName.Location = new System.Drawing.Point(263, 28);
+            this.comboBoxAppName.Name = "comboBoxAppName";
+            this.comboBoxAppName.Size = new System.Drawing.Size(296, 21);
+            this.comboBoxAppName.TabIndex = 0;
+            this.comboBoxAppName.SelectedIndexChanged += new System.EventHandler(this.comboBoxAppName_SelectedIndexChanged);
+            // 
+            // comboBoxDbNames
+            // 
+            this.comboBoxDbNames.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDbNames.FormattingEnabled = true;
+            this.comboBoxDbNames.Location = new System.Drawing.Point(263, 68);
+            this.comboBoxDbNames.Name = "comboBoxDbNames";
+            this.comboBoxDbNames.Size = new System.Drawing.Size(296, 21);
+            this.comboBoxDbNames.TabIndex = 1;
+            this.comboBoxDbNames.SelectedIndexChanged += new System.EventHandler(this.comboBoxDbNames_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Application Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Database Name:";
+            // 
+            // button_save
+            // 
+            this.button_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_save.Location = new System.Drawing.Point(484, 174);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 23);
+            this.button_save.TabIndex = 4;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxUserNames
+            // 
+            this.comboBoxUserNames.FormattingEnabled = true;
+            this.comboBoxUserNames.Location = new System.Drawing.Point(263, 126);
+            this.comboBoxUserNames.Name = "comboBoxUserNames";
+            this.comboBoxUserNames.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxUserNames.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(406, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(263, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "User Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(403, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Password:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 390);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxUserNames);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxDbNames);
+            this.Controls.Add(this.comboBoxAppName);
+            this.Name = "Form1";
+            this.Text = "UltraSuperNoodle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxAppName;
+        private System.Windows.Forms.ComboBox comboBoxDbNames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.ComboBox comboBoxUserNames;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ UltraSuperNoodle/UltraSuperNoodle/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.Administration;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace UltraSuperNoodle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServerManager serverManager = new ServerManager();
+            //var apps;
+            serverManager.Sites.ToList().ForEach(site => site.Applications.Where(app => app.Path != @"/").ToList().ForEach(app => comboBoxAppName.Items.Add(app)));
+
+            SqlDataSourceEnumerator instance = SqlDataSourceEnumerator.Instance;
+            //var x = instance.GetDataSources();
+            //x = instance.GetDataSources();
+
+            //var z = 
+            //string constring = "server=" + x.Rows[0].ItemArray[0] + ";Integrated Security = sspi";
+            string constring = "server=localhost;Integrated Security = sspi";
+            using (var con = new SqlConnection(constring))
+            using (var da = new SqlDataAdapter("SELECT Name FROM master.sys.databases", con))
+            {
+                var ds = new DataSet();
+                da.Fill(ds);
+                var x3 = ds.Tables[0].Rows.Cast<DataRow>().Select(x2 => x2["Name"].ToString());
+                comboBoxDbNames.DataSource = x3.ToList();
+                
+            }
+            
+        }
+
+        private void comboBoxDbNames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string databaseName = (string)((ComboBox)sender).SelectedItem;
+            string constring = "server=localhost;Integrated Security = sspi";
+            using (var con = new SqlConnection(constring))
+                try {
+                    using (var da = new SqlDataAdapter("select name from " + databaseName + ".sys.sysusers where islogin = 1", con))
+                        {
+                            var ds = new DataSet();
+                            da.Fill(ds);
+                            var x3 = ds.Tables[0].Rows.Cast<DataRow>().Select(x2 => x2["Name"].ToString());
+                            comboBoxUserNames.DataSource = x3.ToList();
+
+                        }
+                    }
+            catch(Exception ex)
+                {
+                  
+                }
+        }
+
+        private void comboBoxAppName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Microsoft.Web.Administration.Application applicationName = (Microsoft.Web.Administration.Application)((ComboBox)sender).SelectedItem;
+
+            var physicalPath = applicationName.VirtualDirectories[0].PhysicalPath + @"\web.config";
+
+            XDocument xDoc = XDocument.Load(physicalPath);
+
+            foreach (XElement element in xDoc.Root.Elements())
+            {
+                if (element.Name.LocalName.Equals("connectionStrings"))
+                {
+                    var yy = 5;
+                }
+            }
+
+            
+        }
+    }
+}
