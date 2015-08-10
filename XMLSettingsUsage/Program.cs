b32086533f7dff3de6bc6f151c9ee97040ebb98a
avais88786 <avaisuddin.mohammad@outlook.com>
2015-08-10 23:11:17 +0100

    jjj

---
+++ XMLSettingsUsage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLSettingsUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlWriterSettings xmlSettings = new XmlWriterSettings();
+            //xmlSettings.Indent = true;
+
+            XmlWriter writer = XmlWriter.Create(@"..\..\App_data\data.xml", xmlSettings);
+            writer.WriteStartElement("book");
+            writer.WriteElementString("item", "testing");
+            writer.WriteEndElement();
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
