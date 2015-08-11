75adf3674408904f8dfdfa4d672e22f3a3d6f3b8
avais88786 <avaisuddin.mohammad@outlook.com>
2015-08-11 17:37:14 +0100

    Tuesday

--- OTOMReverseEngineerXML/Program.cs
+++ OTOMReverseEngineerXML/Program.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Fleet;
+using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Tradesman;
+using AutoMapper;
+using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Tradesman.DeclarationQuestions;
+using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Tradesman.BusinessDetailsQuestions;
 
 namespace OTOMReverseEngineerXML
 {
@@ -13,14 +16,42 @@ namespace OTOMReverseEngineerXML
     {
         static void Main(string[] args)
         {
-            XmlSerializer xmlSer = new XmlSerializer(typeof(MiniFleetNBRq));
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanDataCapture>()
+                .ForMember(d => d.DeclarationQuestions, s => s.MapFrom(ss => ss))
+                .ForMember(d => d.BusinessDetails, s => s.MapFrom(ss => ss));
+                //.ForMember(d => d.DeclarationQuestions, src => src.MapFrom(s=> new TradesmanDeclarationQuestionsTab(){TradesmanDeclarationGroup = new TradesmanDeclarationGroup(){UnSpentConvictions = s.Insured.Declaration.ConvictionsInd.Value}} ));
 
-            FileStream fs = new FileStream("RSAQuoteErrors.xml", FileMode.Open);
-            MiniFleetNBRq deser = (MiniFleetNBRq)xmlSer.Deserialize(fs);
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanBusinessDetailsTab>()
+                .ForMember(d => d.SubsidiaryCompaniesGroup, s => s.MapFrom(model => model));
+
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanSubsidiaryCompaniesGroup>()
+                .ForMember(d => d.SubsidiaryCompanies, s => s.MapFrom(model => model));
+
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanSubsidiaryCompaniesLogicalGroup>()
+                .ForMember(d => d.SubsidiaryCompanies, s => s.MapFrom(model => model));
+
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanSubsidiaryCompany>()
+                .ForMember(d => d.CompanyName, s => s.MapFrom(model => model.Insured.CompanyName));
+            
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanDeclarationQuestionsTab>()
+                .ForMember(d => d.TradesmanDeclarationGroup, s => s.MapFrom(model => model));
+
+            Mapper.CreateMap<TradesmanAllNBRq,TradesmanDeclarationGroup>()
+                .ForMember(d => d.DeclaredBankrupt, opt => opt.MapFrom(model => model.Insured.Declaration.BankruptInd.Value));
+
+            
+
+            XmlSerializer xmlSer = new XmlSerializer(typeof(TradesmanAllNBRq));
+
+            FileStream fs = new FileStream(@"XMLs\Tradesman1.xml", FileMode.Open);
+            TradesmanAllNBRq deser = (TradesmanAllNBRq)xmlSer.Deserialize(fs);
             fs.Close();
 
-            FleetDataCapture fleetDC = new FleetDataCapture();
-            fleetDC.BusinessDetails.InsuredDetails.InsuredOrCompanyName = deser.Insured.CompanyName;
+            var x = Mapper.Map<TradesmanAllNBRq, TradesmanDataCapture>(deser);
+
+            var y = new TradesmanDataCapture();
+            y.DeclarationQuestions.TradesmanDeclarationGroup.UnSpentConvictions = deser.Insured.Declaration.ConvictionsInd.Value;
+            
 
 
         }
