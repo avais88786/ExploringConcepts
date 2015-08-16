29a5c5ed96ba19c551aa6109330e135981918288
avais88786 <avaisuddin.mohammad@outlook.com>
2015-08-16 23:25:52 +0100

    Weekend work

---
+++ OTOMReverseEngineerXML/AutoMapperProfiles/TradesmanNBRqProfile.cs
@@ -0,0 +1,100 @@
+﻿using AutoMapper;
+using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Tradesman;
+using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Tradesman.BusinessDetailsQuestions;
+using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Tradesman.DeclarationQuestions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTOMReverseEngineerXML.AutoMapperProfiles
+{
+    public class TradesmanNBRqProfile : BaseProfile
+    {
+        public TradesmanNBRqProfile()
+            : base(typeof(TradesmanAllNBRq), typeof(TradesmanDataCapture))
+        {
+
+        }
+
+        protected override void Configure()
+        {
+            
+            base.Configure();
+
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanDeclarationGroup>()
+                .ForMember(d => d.DeclaredBankrupt, opt => opt.MapFrom(model => model.Insured.Declaration.BankruptInd.Value))
+                .ForMember(d => d.DischargeOfFumes, opt => opt.MapFrom(model => model.Insured.Declaration.DischargeOfWasteInd.Value))
+                .ForMember(d => d.HarmfulSubstances, opt => opt.MapFrom(model => model.Insured.Declaration.HarmfulSubstancesInd.Value))
+                .ForMember(d => d.ProposalRefused, opt => opt.MapFrom(model => model.Insured.Declaration.InsuranceInd.Value))
+                .ForMember(d => d.SeparateDedicatedBusinessPremises, opt => opt.MapFrom(model => model.Insured.Declaration.BusinessPremisesInd.Value))
+                .ForMember(d => d.UnSpentConvictions, opt => opt.MapFrom(model => model.Insured.Declaration.ConvictionsInd.Value));
+
+            Mapper.CreateMap<TradesmanAllNBRq, BusinessDetailsGroup>()
+                .ForMember(d => d.CompanyStatus, s => s.MapFrom(model => model.Insured.CompanyStatusCode))
+                .ForMember(d => d.NumberOfYearsExperience, s => s.MapFrom(model => model.Insured.YearsExperience))
+                .ForMember(d => d.AddressInformation, s => s.MapFrom(model => model.Insured.Address))
+                .ForMember(d => d.TradingName, s => s.MapFrom(model => model.Insured.CompanyName))
+                .ForMember(d => d.TheTradingName, s => s.MapFrom(model => model.Insured.TradingName.Name))
+                .ForMember(d => d.YearEstablished, s => s.MapFrom(model => model.Insured.YearBusinessEstablished))
+                .ForMember(d => d.NumberOfYearsExperience, s => s.MapFrom(model => model.Insured.YearsExperience));
+
+            Mapper.CreateMap<TradesmanAllNBRq, IndividualLogicalGroup>()
+                .ForMember(d => d.Individuals, s => s.MapFrom(model => model.Insured.IndividualName));
+
+            Mapper.CreateMap<TradesmanAllNBRqInsuredIndividualName, TradesmanIndividual>()
+                .ForMember(d => d.FirstName, s => s.MapFrom(model => model.FirstForename))
+                .ForMember(d => d.LastName, s => s.MapFrom(model => model.Surname))
+                .ForMember(d => d.IndividualTitle, s => s.MapFrom(model => model.TitleCode));
+
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanStandardQuestionsGroup>()
+                .ForMember(d => d.CoverStartDate, s => s.MapFrom(model => model.StartDate.Add(model.StartTime.TimeOfDay)))
+                .ForMember(d => d.CurrentPreviousInsurer, s => s.MapFrom(model => model.PriorInsurer.Code))
+                //.ForMember(d => d.InsurerInformation, s => s.MapFrom(model => model.PriorInsurer)); cannot find
+                .ForMember(d => d.TargetPremium, s => s.MapFrom(model => model.Policy.TargetPremium));
+
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanSubsidiaryCompaniesGroup>()
+                .ForMember(d => d.SubsidiaryCompaniesExist, s => s.MapFrom(model => model.Insured.Subsidiary.Count() > 0 ? true : false));
+
+            Mapper.CreateMap<TradesmanAllNBRq, TradesmanSubsidiaryCompaniesLogicalGroup>()
+                .ForMember(d => d.SubsidiaryCompanies, s => s.MapFrom(model => model.Insured.Subsidiary));
+
+            Mapper.CreateMap<TradesmanAllNBRqInsuredSubsidiary, TradesmanSubsidiaryCompany>()
+                .ForMember(d => d.CompanyName, s => s.MapFrom(model => model.CompanyName))
+                .ForMember(d => d.CoveredUnderPolicy, s => s.MapFrom(model => model.SubsidiaryIncludedInd.Value))
+                .ForMember(d => d.EmployersReferenceNumberTwo, s => s.MapFrom(model => model.EmployersRefNo))
+                .ForMember(d => d.ExemptFromHMCEEmployersReferenceNumber, s => s.MapFrom(model => model.PAYEThresholdInd.Value));
+
+            Mapper.CreateMap<TradesmanAllNBRq, GasFitLogicalGroup>()
+                .ForMember(d => d.IsGasFitOrInstallWorkUndertaken, s => s.MapFrom(model => model.Insured.GeneralActivity.GasFittingInstallationInd.Value));
+
+            Mapper.CreateMap<TradesmanAllNBRq, Phase3LogicalGroup>()
+                .ForMember(d => d.IsPhase3ElecWorkUndertaken, s => s.MapFrom(model => model.Insured.GeneralActivity.Phase3ElectricalWorkInd.Value));
+
+            Mapper.CreateMap<TradesmanAllNBRq, MaximumDepthLogicalGroup>()
+                .ForMember(d => d.MaximumDepthWorkUndertakenGroup, s => s.MapFrom(model => model.Insured.GeneralActivity.DepthLimit));
+
+            Mapper.CreateMap<TradesmanAllNBRq, MaximumHeightLogicalGroup>()
+                .ForMember(d => d.MaximumHeightWorkCarriedOut, s => s.MapFrom(model => model.Insured.GeneralActivity.HeightLimit));
+
+            Mapper.CreateMap<TradesmanAllNBRq, TradeDetailsQuestionGroup>()
+                .ForMember(d => d.Trades, s => s.MapFrom(model => model.Insured.Trade));
+            // not there in schema ? //.ForMember(d => d.TradesmanHeatCoverLogicalGroup, s => s.MapFrom(model => model.Insured.Trade));
+
+            Mapper.CreateMap<TradesmanAllNBRqInsuredTrade, TradesmanTrade>()
+                .ForMember(d => d.NumberOfWorkers, s => s.MapFrom(model => model.NoOfWorkers))
+                .ForMember(d => d.PercentageTurnover, s => s.MapFrom(model => model.TurnoverPercent))
+                .ForMember(d => d.TradeType, s => s.MapFrom(model => model.Code))
+                .ForMember(d => d.IsMainTrade, s => s.MapFrom(model => model.MainInd.Value));
+
+            Mapper.CreateMap<TradesmanAllNBRqInsuredTrade, TreatmentDetailsLogicalGroup>()
+                .ForMember(d => d.TreatmentDetails, s => s.MapFrom(model => model.Treatment));
+
+            Mapper.CreateMap<TradesmanAllNBRqInsuredTradeTreatment, TreatmentDetail>()
+                 .ForMember(d => d.NoofWorkers, s => s.MapFrom(model => model.NoOfWorkers))
+                 .ForMember(d => d.TreatmentDetailsType, s => s.MapFrom(model => model.TreatmentCode));
+
+        }
+    }
+}
--- OTOMReverseEngineerXML/Program.cs
+++ OTOMReverseEngineerXML/Program.cs
@@ -12,6 +12,8 @@ using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Tradesman.BusinessDetailsQu
 using OpenGI.MVC.BusinessLines.ViewModels.ViewModels;
 using System.Reflection;
 using System.Linq.Expressions;
+using OTOMReverseEngineerXML.AutoMapperProfiles;
+using OpenGI.MVC.BusinessLines.ViewModels.ViewModels.Fleet;
 
 namespace OTOMReverseEngineerXML
 {
@@ -19,214 +21,63 @@ namespace OTOMReverseEngineerXML
     {
         static void Main(string[] args)
         {
-            ConfigureAndInitialize();
+            Mapper.Initialize(mapper =>
+                {
+                    BaseConfigurations.AutoMapperOneTimeConfigurations();
+                    mapper.AddProfile<TradesmanNBRqProfile>();
+                    mapper.AddProfile<MiniFleetNBRqProfile>();
+                });
 
-            Mapper.CreateMap<TradesmanAllNBRq, BusinessDetailsGroup>()
-                .ForMember(d => d.CompanyStatus, s => s.MapFrom(model => model.Insured.CompanyStatusCode))
-                .ForMember(d => d.NumberOfYearsExperience, s => s.MapFrom(model => model.Insured.YearsExperience))
-                .ForMember(d => d.AddressInformation, s => s.MapFrom(model => model.Insured.Address));
+            XmlSerializer xmlSer;
+            FileStream fs;
+            object x;
+            StringBuilder sbuilder;
+             StringWriter sWriter ;
+            string y ;
+            object deser; 
 
-            Mapper.CreateMap<TradesmanAllNBRq, IndividualLogicalGroup>()
-                .ForMember(d => d.Individuals, s => s.MapFrom(model => model.Insured.IndividualName));
+            #region Tradesman Just UnComment Me
 
-            Mapper.CreateMap<TradesmanAllNBRq, TradesmanSubsidiaryCompaniesLogicalGroup>()
-                .ForMember(d => d.SubsidiaryCompanies, s => s.MapFrom(model => model.Insured.Subsidiary));
+            xmlSer = new XmlSerializer(typeof(TradesmanAllNBRq));
 
-            Mapper.CreateMap<TradesmanAllNBRqInsuredSubsidiary, TradesmanSubsidiaryCompany>()
-                .ForMember(d => d.CompanyName, s => s.MapFrom(model => model.CompanyName));
-
-            Mapper.CreateMap<TradesmanAllNBRq, TradesmanDeclarationGroup>()
-                .ForMember(d => d.DeclaredBankrupt, opt => opt.MapFrom(model => model.Insured.Declaration.BankruptInd.Value))
-                .ForMember(d => d.DischargeOfFumes, opt => opt.MapFrom(model => model.Insured.Declaration.DischargeOfWasteInd.Value))
-                .ForMember(d => d.HarmfulSubstances, opt => opt.MapFrom(model => model.Insured.Declaration.HarmfulSubstancesInd.Value))
-                .ForMember(d => d.ProposalRefused, opt => opt.MapFrom(model => model.Insured.Declaration.InsuranceInd.Value))
-                .ForMember(d => d.SeparateDedicatedBusinessPremises, opt => opt.MapFrom(model => model.Insured.Declaration.BusinessPremisesInd.Value))
-                .ForMember(d => d.UnSpentConvictions, opt => opt.MapFrom(model => model.Insured.Declaration.ConvictionsInd.Value));
-
-            
-
-            XmlSerializer xmlSer = new XmlSerializer(typeof(TradesmanAllNBRq));
-
-            FileStream fs = new FileStream(@"XMLs\Tradesman1.xml", FileMode.Open);
-            TradesmanAllNBRq deser = (TradesmanAllNBRq)xmlSer.Deserialize(fs);
+            fs = new FileStream(@"XMLs\Tradesman1.xml", FileMode.Open);
+            deser = (TradesmanAllNBRq)xmlSer.Deserialize(fs);
             fs.Close();
 
-            var x = Mapper.Map<TradesmanAllNBRq, TradesmanDataCapture>(deser);
+            x = Mapper.Map<TradesmanAllNBRq, TradesmanDataCapture>((TradesmanAllNBRq)deser);
 
             xmlSer = new XmlSerializer(typeof(TradesmanDataCapture));
-            
-            StringBuilder sbuilder = new StringBuilder();
-            StringWriter sWriter = new StringWriter(sbuilder);
+
+            sbuilder = new StringBuilder();
+            sWriter = new StringWriter(sbuilder);
             xmlSer.Serialize(sWriter, x);
-            var y = sbuilder.ToString();
+            y = sbuilder.ToString();
+
+            #endregion
+
+            #region Fleet 
+
+            xmlSer = new XmlSerializer(typeof(MiniFleetNBRq));
+
+            fs = new FileStream(@"XMLs\RSAQuoteErrors.xml", FileMode.Open);
+            deser = (MiniFleetNBRq)xmlSer.Deserialize(fs);
+            fs.Close();
+
+            x = Mapper.Map<MiniFleetNBRq, FleetDataCapture>((MiniFleetNBRq)deser);
+
+            xmlSer = new XmlSerializer(typeof(FleetDataCapture));
+
+            sbuilder = new StringBuilder();
+            sWriter = new StringWriter(sbuilder);
+            xmlSer.Serialize(sWriter, x);
+            y = sbuilder.ToString();
+
+
+            #endregion
+
 
         }
-
-        private static void ConfigureAndInitialize()
-        {
-            Mapper.Initialize(cfg =>
-            {
-                cfg.ReplaceMemberName("Line1", "AddressLineOne");
-                cfg.ReplaceMemberName("Line2", "AddressLineTwo");
-                cfg.ReplaceMemberName("Line3", "AddressLineThree");
-                cfg.ReplaceMemberName("Line4", "AddressLineFour");
-            });
-
-            CreateNestedMappers(typeof(TradesmanAllNBRq), typeof(TradesmanDataCapture));
-            ParseSourceType(typeof(TradesmanAllNBRq), typeof(TradesmanDataCapture));
-        }
-
-        private static void ParseSourceType(Type sourceType, Type destinationType)
-        {
-            PropertyInfo[] sourceProperties = sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var sourceProperty in sourceProperties)
-            {
-                Type sourcePropertyType = sourceProperty.PropertyType;
-                if (Filter(sourcePropertyType))
-                    continue;
-
-                if (sourceProperty.Name.Contains("Address"))
-                {
-                    Mapper.CreateMap(sourcePropertyType, typeof(AddressInformation));
-                    continue;
-                }
-
-                if (sourceProperty.Name.Contains("Code"))
-                {
-                    Mapper.CreateMap(sourcePropertyType, typeof(CodeList)).ConvertUsing<ToCodeListConverter>();
-                }
-
-                if (sourcePropertyType.IsArray)
-                {
-                    ParseSourceType(sourcePropertyType.GetElementType(), destinationType);
-                }
-
-                if (sourcePropertyType.IsClass)
-                {
-                    ParseSourceType(sourcePropertyType, destinationType);
-                }
-            }
-        }
-
-        public static void CreateNestedMappers(Type sourceType, Type destinationType)
-        {
-            PropertyInfo[] sourceProperties = sourceType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            PropertyInfo[] destinationProperties = destinationType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var destinationProperty in destinationProperties)
-            {
-                Type destinationPropertyType = destinationProperty.PropertyType;
-                if (Filter(destinationPropertyType))
-                    continue;
-
-                if (destinationProperty.PropertyType.IsClass && !(destinationProperty.PropertyType.IsGenericType && destinationProperty.PropertyType.GetGenericTypeDefinition() == typeof(List<>))) {
-
-                    var mi = typeof(Program).GetMethod("GenericsMap").MakeGenericMethod(sourceType, destinationType);
-                    mi.Invoke(null, new object[] { destinationProperty.Name });
-
-                        if (destinationProperty.PropertyType.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance).Count() == 1 && destinationProperty.PropertyType.GetProperties().First().PropertyType.IsClass){
-                        {
-                                CreateMappers(sourceType, destinationPropertyType);
-                        }
-                    }
-
-                    CreateNestedMappers(sourceType, destinationProperty.PropertyType);
-                }
-
-
-                PropertyInfo sourceProperty = sourceProperties.FirstOrDefault(prop => NameMatches(prop.Name, destinationProperty.Name));
-                Type sourcePropertyType;
-                if (sourceProperty == null) {
-                    continue;
-                }
-
-                sourcePropertyType = sourceProperty.PropertyType;
-                if (destinationPropertyType.IsGenericType)
-                {
-                    Type destinationGenericType = destinationPropertyType.GetGenericArguments()[0];
-                    if (Filter(destinationGenericType))
-                        continue;
-                    Type sourceGenericType;
-                    
-                    if (sourcePropertyType.IsArray)
-                        sourceGenericType = sourcePropertyType.GetElementType();
-                    else
-                        sourceGenericType = sourcePropertyType.GetGenericArguments()[0];
-                    CreateMappers(sourceGenericType, destinationGenericType);
-                }
-                else
-                {
-                    CreateMappers(sourcePropertyType, destinationPropertyType);
-                }
-
-                
-
-                Mapper.CreateMap(sourceType, destinationType);
-            }
-
-            Mapper.CreateMap(sourceType, destinationType);
-
-        }
-
-        public static void GenericsMap<T1, T2>(string destPropertyName)
-        {
-            //http://www.crowbarsolutions.com/dynamically-generating-lambda-expressions-at-runtime-from-properties-obtained-through-reflection-on-generic-types/
-            //http://stackoverflow.com/questions/8315819/expression-lambda-and-query-generation-at-runtime-simplest-where-example
-            //http://www.andymcm.com/blog/2009/09/building-lambda-expressions-at-runtime.html
-            //http://www.codeproject.com/Articles/17575/Lambda-Expressions-and-Expression-Trees-An-Introdu
-
-            //Mapper.CreateMap<TradesmanAllNBRq, TradesmanDataCapture>();
-            //.ForMember(d => d.DeclarationQuestions, s => s.MapFrom(ss => ss))
-
-
-            //Mapper.CreateMap<T1, T2>();
-
-            var destparameterExpression = Expression.Parameter(typeof(T2), "dest");
-            var destmemberExpression = Expression.PropertyOrField(destparameterExpression, destPropertyName);
-            var destmemberExpressionConversion = Expression.Convert(destmemberExpression, typeof(object));
-            var destlambda = Expression.Lambda<Func<T2, object>>(destmemberExpressionConversion, destparameterExpression);
-
-
-            var srcparameterExpression = Expression.Parameter(typeof(T1), "src");
-            //var srcmemberExpression = Expression.Property()
-            var srcmemberExpressionConversion = Expression.Convert(srcparameterExpression, typeof(object));
-            var srclambda = Expression.Lambda<Func<T1, object>>(srcmemberExpressionConversion, srcparameterExpression);
-
-            Mapper.CreateMap<T1, T2>()
-             .ForMember(destlambda, s => s.MapFrom(srclambda));
-
-        }
-
-        private static void CreateMappers(Type sourcePropertyType, Type destinationPropertyType)
-        {
-            Mapper.CreateMap(sourcePropertyType, destinationPropertyType);
-        }
-
-
-        static bool Filter(Type type)
-        {
-            return type.IsPrimitive || NoPrimitiveTypes.Contains(type.Name);
-        }
-
-        static readonly HashSet<string> NoPrimitiveTypes = new HashSet<string>() { "String", "DateTime", "Decimal" };
-
-        private static bool NameMatches(string memberName, string nameToMatch)
-        {
-            return String.Compare(memberName, nameToMatch, StringComparison.OrdinalIgnoreCase) == 0;
-        }
-
 
     }
-
-    public class ToCodeListConverter : ITypeConverter<object, CodeList>
-    {
-
-        public CodeList Convert(ResolutionContext context)
-        {
-            var valueProperty = context.SourceType.GetProperty("Value");
-            var shortDescrProperty = context.SourceType.GetProperty("ShortDescription");
-            return new CodeList() { SelectedDescription = (string)shortDescrProperty.GetValue(context.SourceValue), SelectedValue = (string)valueProperty.GetValue(context.SourceValue) };
-        }
-    }
-
 
 }
